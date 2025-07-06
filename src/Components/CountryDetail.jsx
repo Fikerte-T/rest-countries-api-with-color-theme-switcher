13e6ea5eb561102808d3521cdcb546e9b80abba2
fikerte <tofikertesfaye@gmail.com>
2025-07-06 17:05:39 +0300

    error handling

--- src/Components/CountryDetail.jsx
+++ src/Components/CountryDetail.jsx
@@ -18,28 +18,28 @@ const CountryDetail = () => {
         <Link className='block w-fit h-10 my-14 mx-10 md:mx-20 px-5 py-2 shadow rounded-md text-lm-input dark:text-custom-white dark:bg-dm-elements  hover:text-dm-bg dark:hover:bg-lm-bg' to={'/'}>
         <img src={isDarkMode ? "/back-white.svg" : "/back.svg"} className='float-left w-5 h-5 mr-3' alt="back icon" />
         Back</Link>
-        <div className='flex flex-col md:flex-row justify-between px-5 md:px-20 dark:text-custom-white '  >
+        <div className='flex flex-col md:flex-row justify-between items-center px-5 md:px-20 dark:text-custom-white '  >
             {country ? (
         <>
-            <div className='w-full md:w-[60%] lg:w-[45%]'>
-                <img className='h-full' src={country.flags.svg} alt="" />
+            <div className='w-full md:w-[50%] lg:w-[45%]'>
+                <img className='' src={country?.flags.svg || 'None'} alt="" />
             </div>
-            <div className='w-full md:w-[30%] lg:w-[45%] py-10'>
-                <h2 className='text-2xl font-bold mb-5'>{country.name}</h2>
+            <div className='w-full md:w-[40%] lg:w-[45%] py-10'>
+                <h2 className='text-2xl font-bold mb-5'>{country?.name}</h2>
 
-                <div className='flex flex-col items-baseline space-y-10 lg:flex-row lg:justify-between '>
+                <div className='flex flex-col items-baseline space-y-10 lg:flex-row lg:justify-between lg:space-x-5 '>
                     <div className='flex flex-col space-y-2'>
-                        <p className='font-semibold'>Native Name: <span className='font-light'>{country.nativeName}</span></p>
-                        <p className='font-semibold'>Population: <span className='font-light'>{country.population.toLocaleString()}</span></p>
-                        <p className='font-semibold'>Region: <span className='font-light'>{country.region}</span></p>
-                        <p className='font-semibold'>Sub Region: <span className='font-light'>{country.subregion}</span></p>
-                        <p className='font-semibold'>Capital: <span className='font-light'>{country.capital}</span></p>
+                        <p className='font-semibold'>Native Name: <span className='font-light'>{country?.nativeName || 'None'}</span></p>
+                        <p className='font-semibold'>Population: <span className='font-light'>{country?.population.toLocaleString() || 'None'}</span></p>
+                        <p className='font-semibold'>Region: <span className='font-light'>{country?.region || 'None'}</span></p>
+                        <p className='font-semibold'>Sub Region: <span className='font-light'>{country?.subregion || 'None'}</span></p>
+                        <p className='font-semibold'>Capital: <span className='font-light'>{country?.capital || 'None'}</span></p>
                     </div>
                     <div className='flex flex-col space-y-2'>
-                        <p className='font-semibold'>Top Level Domain: <span className='font-light'>{country.topLevelDomain}</span></p>
-                        <p className='font-semibold'>Currencies: <span className='font-light'>{country.currencies[0].name}</span></p>        
+                        <p className='font-semibold'>Top Level Domain: <span className='font-light'>{country?.topLevelDomain || 'None'}</span></p>
+                        <p className='font-semibold'>Currencies: <span className='font-light'>{country.currencies?.[0]?.name || 'None'}</span></p>        
                         <p className='font-semibold'>Languages: 
-                            {country.languages.map((l, i) => (
+                            {country.languages && country.languages.map((l, i) => (
                                 <span key={i} className='font-light ml-1'>
                                     {l.name}{i < country.languages.length - 1 ? ',' : ''}
                                 </span>
